af66282dac93455e3d45f0b9f900f2d4e5505b81
Brook <brook-tamrat.kifle@univ-rouen.fr>
2025-11-05 11:26:11 +0100

    TP Graphiz Q2.3

--- TP_Graphviz/run1.sh
+++ TP_Graphviz/run1.sh
@@ -1,7 +1,6 @@
-# tool=dot
-# $tool -Tpng $1.dot >$1.png
-# $tool -Tsvg $1.dot >$1.svg
+#!/bin/sh
 
+# tool=dot
 tool=neato
-$tool -Tpng $1.dot >$1.png
-$tool -Tsvg $1.dot >$1.svg
+$tool -Tpng TP_Graphviz/$1.dot >$1.png
+$tool -Tsvg TP_Graphviz/$1.dot >$1.svg
